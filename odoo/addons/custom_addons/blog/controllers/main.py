77c1a774b04a0c9c44eef8a56980158f0b6148ef
18eldar05 <eldareldarosss@gmail.com>
2025-01-10 08:26:26 +0300

    handling errors, raw requests

--- odoo/addons/custom_addons/blog/controllers/main.py
+++ odoo/addons/custom_addons/blog/controllers/main.py
@@ -4,40 +4,31 @@ from odoo.http import request
 
 class Blog(http.Controller):
 
-    @http.route('/blog/attribute', auth='public', csrf=False, type='http', methods=['POST'])
+    @http.route('/blog/attribute', auth='user', csrf=False, type='http', methods=['POST'])
     def create_attribute(self, **kw):
         try:
-            print("HERE", kw)
-            data = {
-                'name': kw['name'],
-                'type': kw['type'],
-                'data_type': kw['data_type'],
-                'required': bool(kw['required']),
-                'length': int(kw['length']),
-            }
+            data = request.get_json_data()
             attribute = request.env['blog.attribute'].sudo().create(data)
             data.update({'id': attribute.id})
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=400)
+            return request.make_json_response(str(e), status=500)
         return request.make_json_response(data, headers={"Content-Type": "application/json"}, cookies=None, status=200)
 
-    @http.route('/blog/document', auth='public', csrf=False, type='http', methods=['POST'])
+    @http.route('/blog/document', auth='user', csrf=False, type='http', methods=['POST'])
     def create_document(self, **kw):
         try:
-            data = {
-                'name': kw['name'],
-                'description': kw['description'],
-                'version': float(kw['version']),
-                'active': bool(kw['active']),
-                'attribute_ids': [int(x) for x in kw['attribute_ids'].split()],
-            }
+            data = request.get_json_data()
             document = request.env['blog.document'].sudo().create(data)
             data.update({'id': document.id})
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=400)
+            return request.make_json_response(str(e), status=500)
         return request.make_json_response(data, headers={"Content-Type": "application/json"}, cookies=None, status=200)
 
-    @http.route('/blog/attribute/get_all', auth='public', csrf=False, type='json', methods=['GET'])
+    @http.route('/blog/attribute/get_all', auth='user', csrf=False, type='json', methods=['GET'])
     def get_all_attributes(self, **kw):
         attributes = request.env['blog.attribute'].sudo().search([])
         attributes_list = []
@@ -53,7 +44,7 @@ class Blog(http.Controller):
             })
         return attributes_list
 
-    @http.route('/blog/document/get_all', auth='public', csrf=False, type='json', methods=['GET'])
+    @http.route('/blog/document/get_all', auth='user', csrf=False, type='json', methods=['GET'])
     def get_all_documents(self, **kw):
         documents = request.env['blog.document'].sudo().search([])
         documents_list = []
@@ -69,12 +60,15 @@ class Blog(http.Controller):
             })
         return documents_list
 
-    @http.route('/blog/attribute', auth='public', csrf=False, type='http', methods=['GET'])
+    @http.route('/blog/attribute', auth='user', csrf=False, type='http', methods=['GET'])
     def get_attribute(self, **kw):
         try:
-            attribute = request.env['blog.attribute'].sudo().search([('id', '=', int(kw['id']))])
+            attribute_id = request.get_json_data().get('id')
+            attribute = request.env['blog.attribute'].sudo().search([('id', '=', attribute_id)])
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=404)
+            return request.make_json_response(str(e), status=404)
         data = {
             'uuid': attribute.uuid,
             'name': attribute.name,
@@ -85,12 +79,15 @@ class Blog(http.Controller):
         }
         return request.make_json_response(data, headers={"Content-Type": "application/json"}, cookies=None, status=200)
 
-    @http.route('/blog/document', auth='public', csrf=False, type='http', methods=['GET'])
+    @http.route('/blog/document', auth='user', csrf=False, type='http', methods=['GET'])
     def get_document(self, **kw):
         try:
-            document = request.env['blog.document'].sudo().search([('id', '=', int(kw['id']))])
+            document_id = request.get_json_data().get('id')
+            document = request.env['blog.document'].sudo().search([('id', '=', document_id)])
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=404)
+            return request.make_json_response(str(e), status=404)
         data = {
             'uuid': document.uuid,
             'name': document.name,
@@ -101,12 +98,15 @@ class Blog(http.Controller):
         }
         return request.make_json_response(data, headers={"Content-Type": "application/json"}, cookies=None, status=200)
 
-    @http.route('/blog/document/update', auth='public', csrf=False, type='http', methods=['POST'])
+    @http.route('/blog/document/update', auth='user', csrf=False, type='http', methods=['POST'])
     def update_document(self, **kw):
         try:
-            document = request.env['blog.document'].sudo().search([('id', '=', int(kw['id']))])
+            document_id = request.get_json_data().get('id')
+            document = request.env['blog.document'].sudo().search([('id', '=', document_id)])
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=404)
+            return request.make_json_response(str(e), status=404)
         data = {
             'name': document['name'],
             'description': document['description'],
@@ -118,6 +118,8 @@ class Blog(http.Controller):
             request.env['blog.document'].sudo().create(data)
             data['attribute_ids'] = [attribute_id.id for attribute_id in document.attribute_ids]
             document.write({'active': False})
+        except (ValueError, TypeError, AttributeError) as e:
+            return request.make_json_response(str(e), status=400)
         except Exception as e:
-            return request.make_json_response(repr(e), headers={"Content-Type": "application/json"}, cookies=None, status=400)
+            return request.make_json_response(str(e), status=500)
         return request.make_json_response(data, headers={"Content-Type": "application/json"}, cookies=None, status=200)
